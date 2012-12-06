233d793358b38739d64ef884553103c2c1063b0a
drvinceknight <vincent.knight@gmail.com>
2012-12-06 13:13:35 +0000

    Commited before branch to check for monotonicity of game

--- Shappy/Shap.py
+++ Shappy/Shap.py
@@ -22,7 +22,8 @@ def characteristic_function_check(player_list,characteristic_function):
     for e in power_set(player_list):
         if ",".join(e) not in characteristic_function:
             return False
-    #Need include check that dictionary is superadditive
+    #Need include check that dictionary is Monotone
+    for in e power_set(pl
     return r
 
 def predecessors(player,player_permutation):
@@ -104,8 +105,8 @@ class Coop_Game():
 pl=["A","B","C"]
 #cf={"A":.4,"B":.4,"C":.2,"A,B":.4,"B,C":.4,"A,C":.1,"A,B,C":1}
 #cf={"A":1/3,"B":1/3,"C":1/3,"A,B":2/3,"B,C":2/3,"A,C":2/3,"A,B,C":1}
-#cf={"A":40,"B":40,"C":20,"A,B":70,"B,C":60,"A,C":40,"A,B,C":100}
-cf={"A":40,"B":40,"C":20,"A,B":70,"B,C":40,"A,C":60,"A,B,C":100}
+cf={"A":40,"B":40,"C":20,"A,B":70,"B,C":58,"A,C":40,"A,B,C":100}
+#cf={"A":40,"B":40,"C":20,"A,B":70,"B,C":40,"A,C":60,"A,B,C":100}
 a=Coop_Game(pl,cf)
 print a.valid
 print a.shapley()
